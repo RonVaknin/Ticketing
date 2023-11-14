51c26c586c8b83db48d889a125948d38c9d9fe83
Ronvaknin <57563969+RonVaknin@users.noreply.github.com>
2023-11-14 20:48:53 +0200

    init error handler

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error('An error occured: ', err);
+
+    res.status(400).send({
+        message: 'something went wrong'
+    });
+}
